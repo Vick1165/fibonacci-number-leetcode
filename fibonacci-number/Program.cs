6bb964f41103beece35f72f9e2b004c9e9dfaf9c
Balaji Kumar <vickykumar538@gmail.com>
2022-08-15 20:50:12 +0530

    Add project files.

---
+++ fibonacci-number/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace fibonacci_number
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(fibonacci(4));
+        }
+
+
+        public static int fibonacci(int N)
+        {
+            if (N <= 1)
+                return N;
+
+            int a = 0, b = 1;
+
+            for (int i = 2; i <= N; i++)
+            {
+                int sum = a + b;
+                a = b;
+                b = sum;
+            }
+            return b;
+        }
+    }
+}
